6611db1c193e844b0879444f1dd653dbe1ae4a2f
Admin <reanajb@gmail.com>
2023-12-18 10:53:42 -0500

    updates

--- main.js
+++ main.js
@@ -7,30 +7,52 @@ function isEvenlyDivisible(num1, num2) {
 }
 
 function halfSquare(num1) {
- let num2 = (num1 ** 2) / 2
+ //return (num1 ** 2) / 2
+  let num2 = (num1 ** 2) / 2
     return num2
 }
 
 function isLong(str) {
+  //return num.length >= 15
   if(str.length >= 15){
     return true
-  }else{
+  }else{  //else if(str.length < 15){}
     return false
   }
 }
 
-function exclaim(str) {
- let added = ''
- for(let i =0; i < str.length; i++){
-  if(str[i] !== '!'){
-    added += str[i]}
-      else if(str[i] === "!")
-      added += str 
-    else if(str[i] === '!!!!!!')
-        added += str[i]
-    } return added
-  }
 
+//----------------------------------------------------------------------
+//MY CODE
+
+// function exclaim(str) {
+//  let added = ''
+//  for(let i =0; i < str.length; i++){
+//   if(str[i] !== '!'){
+//     added += str[i]}
+//       else if(str[i] === "!")
+//       added += str 
+//     else if(str[i] === '!!!!!!')
+//         added += str[i]
+//     } return added
+//   }
+
+  // STEVEN's answer
+  // function exclaim(str){
+  //   if(str[str.length -1 !== '!'])
+  //   return str + '!'
+  // }else{
+  //   let str2 = ""
+  //   for(let char of str){
+  //     if(char !== '!')
+  //     str2 = str2 + char
+  //   } return str2 + '!'
+  // }
+
+  function exclaim(str){
+    return str.split('!')[0] + '!'
+  }
+//-----------------------------------------------------------------------------
 
 
 function countWords(str) {
@@ -38,50 +60,108 @@ function countWords(str) {
   }
   
 
+//MY CODE------------------------------------------------------------------------
+// function containsDigit(str) {
+//   let num = [0, 1, 2, 3, 4]
+//   if(typeof str === num){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
 
-function containsDigit(str) {
-  let num = [0, 1, 2, 3, 4]
-  if(typeof str === num){
-    return true
-  }else{
-    return false
-  }
-}
-
-
-function containsLowerCase(str) {
-    for(let i =0; i < str.length; i++){
-      if(str[i] === str[i].toLowerCase() &&
-       str[i] !== str.toUpperCase()) {
-        return true;
-       }else{
-        return false
-       }
+//CORRECT ANSWER
+function containsDigit(str){
+  let num = '0123456789'
+  for(let char of str){
+    if(num.includes(char)){
+      return true
     }
+  } return false
 }
 
+// return /\d/.test(str) --> REGEX
+//---------------------------------------------------------------------------
 
-function containsUpperCase(str) {
-    for(let i =0; i < str.length; i++){
-      if(str[i] === str[i].toUpperCase() &&
-       str[i] !== str.toLowerCase()) {
-        return true;
-       }else{
-        return false
-       }
+//MY CODE
+// function containsLowerCase(str) {
+//     for(let i =0; i < str.length; i++){
+//       if(str[i] === str[i].toLowerCase() &&
+//        str[i] !== str.toUpperCase()) {
+//         return true;
+//        }else{
+//         return false
+//        }
+//     }
+// }
+
+//CORRECT ANSWER
+function containsLowerCase(str){
+    return str.toUpperCase() !== str
+}
+
+//STEVEN'S WAY
+//function containsLowerCase(str){
+//for(let char of str){
+//if(char === char.containsUpperCase){
+  //return true
+// }
+//} return false
+
+
+
+//------------------------------------------------------------------------------
+//MY CODE
+// function containsUpperCase(str) {
+//     for(let i =0; i < str.length; i++){
+//       if(str[i] === str[i].toUpperCase() &&
+//        str[i] !== str.toLowerCase()) {
+//         return true;
+//        }else{
+//         return false
+//        }
+//     }
+// }
+
+
+//correct way
+function containsUpperCase(str){
+return str.toLowerCase() !== str
+}
+
+//------------------------------------------------------------------------------
+//MY CODE
+// function containsNonAlphanumeric(str) {
+//   let char = '!@#$%^&*'  
+//   for(let char1 of str){
+//       if(char.includes(char1)){
+//         return 
+//       }
+// }
+// }
+
+function containsNonAlphanumeric(str){
+  let alphaNum = '012345678abcdefghijklmnopqrstuvwxyz9'
+  for(let char of str){
+    if(!alphaNum.includes(char.toLowerCase())){
+      return true
     }
+  } return false
 }
 
-
-
-function containsNonAlphanumeric(str) {
-    for(let i =0; i < str.length; i++){
-      if(str === "!@$^&*!"){
-        return str.push(str)
-      }
-}
-}
-
+//SAMS WAY
+// let count = 0
+// for(let char of str){
+//   if(containsLowerCase(char) === true){
+//     count = count + 1
+//   }else if(containsUpperCase(char) === true){
+//     count = count + 1
+//   }else if(containsDigit(char) === true){
+//     count = count + 1
+//   }return count !== str.length
+// }
+// }
+//----------------------------------------------------------------------------
 
 
 function containsSpace(str) {
@@ -95,9 +175,11 @@ if(str.includes(" ")){
 // includes returns a true or false if it contains that character you are searching for
 
 
-function digits() {
-  
+function digits(str) {
+
 }
+
+
 
 function truncate() {
 
@@ -116,13 +198,6 @@ function isValidPassword(pass) {
 } return false 
 }
 
-  // if(pass !== pass.toUpperCase() &&
-  //     pass !== pass.toLowerCase()){
-  //       return true
-  //     }else{
-  //       return false
-  //     }
-  //   }
 
 function onlyPunchy() {
 
